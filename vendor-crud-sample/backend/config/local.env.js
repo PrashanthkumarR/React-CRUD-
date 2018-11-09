8704c8b4392c230ef97b0efaae1afdb791b84afa
PrashanthkumarR <prashanthninu@gmail.com>
2018-11-09 13:50:46 +0530

    intial commite

---
+++ vendor-crud-sample/backend/config/local.env.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+
+    secrets: {
+        session: "node-rest-shop-sz01a.mongodb.net",
+        expiresIn: 2629746000
+    },
+
+    db:{
+        URI:" mongodb+srv://prashanth:1SYU4tJRGecFYBon@node-rest-shop-sz01a.mongodb.net/test?retryWrites=true"
+    }
+}
